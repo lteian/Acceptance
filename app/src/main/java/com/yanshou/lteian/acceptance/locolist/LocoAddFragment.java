6ceaf389334b0acd9158090f583274b97e39ea3e
lteian <lteian@126.com>
2018-07-31 17:18:27 +0800

    完善图片存储方案

--- app/src/main/java/com/yanshou/lteian/acceptance/locolist/LocoAddFragment.java
+++ app/src/main/java/com/yanshou/lteian/acceptance/locolist/LocoAddFragment.java
@@ -1,10 +1,17 @@
 package com.yanshou.lteian.acceptance.locolist;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.InputFilter;
+import android.text.InputType;
+import android.text.Layout;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,10 +20,16 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.qmuiteam.qmui.widget.dialog.QMUIDialog;
+import com.qmuiteam.qmui.widget.grouplist.QMUICommonListItemView;
+import com.qmuiteam.qmui.widget.popup.QMUIListPopup;
 import com.yanshou.lteian.acceptance.joblist.LocoInformationActivity;
 import com.yanshou.lteian.acceptance.R;
 import com.yanshou.lteian.acceptance.data.LocoLoco;
 import com.yanshou.lteian.acceptance.data.LocoLocoDao;
+import com.yanshou.lteian.classificationkeyboardview.ClassificationKeyboardViewEditText;
+
+import org.w3c.dom.Text;
 
 public class LocoAddFragment extends Fragment{
     private Button button;
@@ -28,6 +41,60 @@ public class LocoAddFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         final View view=inflater.inflate(R.layout.fragment_loco_add,null);
         locoCF =view.findViewById(R.id.loco_classification_editText);
+        locoCF.setTextDirection(View.TEXT_DIRECTION_RTL);
+        locoCF.setOnFocusChangeListener(new View.OnFocusChangeListener(){
+
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(locoCF.hasFocus()){
+                    locoCF.setText("");
+                }
+            }
+        });
+
+        //设置itemlist内容
+        final QMUICommonListItemView locoTypeListView = view.findViewById(R.id.loco_type_listview);
+        locoTypeListView.setText("车型");
+        locoTypeListView.setDetailText("请选择");
+        locoTypeListView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String[] items = getResources().getStringArray(R.array.LocomotiveModel);
+                QMUIDialog.CheckableDialogBuilder locoCheckableDialog = new QMUIDialog.CheckableDialogBuilder(getContext());
+                locoCheckableDialog.setCheckedIndex(1).addItems(items, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        locoTypeListView.setDetailText(items[which]);
+                        dialog.dismiss();
+                    }
+                }).create().show();
+            }
+        });
+
+        //设置车号EditText
+        QMUICommonListItemView locoNoText = view.findViewById(R.id.loco_no_text);
+        locoNoText.setText(getString(R.string.loco_number));
+        final EditText locoNoEditText = new EditText(getContext());
+        locoNoEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(4)});
+        locoNoEditText.setInputType(InputType.TYPE_CLASS_NUMBER);
+        locoNoEditText.setGravity(Gravity.FILL);
+        locoNoEditText.setBackground(getResources().getDrawable(R.drawable.qmui_divider_bottom_bitmap));
+        locoNoEditText.setFocusable(true);
+        locoNoEditText.setBackgroundColor(Color.WHITE);
+        locoNoEditText.setTextDirection(View.TEXT_DIRECTION_RTL);
+        locoNoEditText.setHint("请输入4位机车号");
+        locoNoEditText.setTextSize(16);
+        locoNoText.addAccessoryCustomView(locoNoEditText);
+        locoNoEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (locoNoEditText.hasFocus()) {
+                    locoNoEditText.setText("");
+                }
+            }
+        });
+
+
         //  按钮传参
         button = view.findViewById(R.id.add_loco_submit);
         button.setOnClickListener(new View.OnClickListener() {
@@ -37,11 +104,9 @@ public class LocoAddFragment extends Fragment{
 
                 //        提取机车型号、车号、修程，保存至post_loco数组
                 //1.提取机车型号
-                Spinner spinner = view.findViewById(R.id.spinner_LocomotiveModel);
-                loco.setLocoType(String.valueOf(spinner.getSelectedItem()));
+                loco.setLocoType(String.valueOf(locoTypeListView.getDetailText()));
                 //        2.提取车号
-                EditText locoNo = view.findViewById(R.id.loco_no);
-                loco.setLocoNumber(String.valueOf(locoNo.getText()));
+                loco.setLocoNumber(String.valueOf(locoNoEditText.getText()));
 //                          3.提取修程
 
                 loco.setLocoClassification(String.valueOf(locoCF.getText()));
